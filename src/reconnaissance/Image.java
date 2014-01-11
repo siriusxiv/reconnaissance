656103eb4498b02aa78f6cfdcbac49079e5e1d9e
SiriusXIV <siriusxiv0@gmail.com>
2014-01-11 16:52:14 +0100

    Calcul du minimum de norm terminé

--- src/reconnaissance/Image.java
+++ src/reconnaissance/Image.java
@@ -62,7 +62,7 @@ public class Image {
 	}
 	
 	/**
-	 * "allume" le pixel de coordonnees (x,y) avec la valeur value.
+	 * "Allume" le pixel de coordonnees (x,y) avec la valeur value.
 	 * de points traces
 	 * @param x
 	 * @param y
@@ -70,5 +70,53 @@ public class Image {
 	public void setPixel(int x,int y, int value) {
 		byte[] pixels = (byte[]) image.getProcessor().getPixels();
 		pixels[y*image.getWidth()+x] =  (byte)value;
+	}
+	/**
+	 * Permet d'obtenir la valeur d'un pixel (entre 0 et 255)
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	public int getPixel(int x, int y){
+		return image.getPixel(x, y)[0];
+	}
+	
+	/**
+	 * Calcule la norme entre le motif et l'image au point x et y.
+	 * @param x
+	 * @param y
+	 * @param motif
+	 * @return
+	 */
+	public int SSD(int x, int y, Motif motif){
+		int res = 0;
+		for(int i = 0; i<motif.getWidth() ; i++){
+			for(int j = 0; j<motif.getHeight() ; j++){
+				int dist = motif.getPixel(i, j)-this.getPixel(x+i, y+j);
+				res+=dist*dist;
+			}
+		}
+		return res;
+	}
+	
+	/**
+	 * Renvoie la position sur l'image où le motif matche le plus.
+	 * @param motif
+	 * @return Un couple d'int : l'abscisse et l'ordonnée x de l'image
+	 */
+	public int[] minSSD(Motif motif){
+		int[] res = new int[2];
+		res[0]=0; res[1]=0;
+		int min = this.SSD(0, 0, motif);
+		for(int i = 0; i<this.getWidth()-motif.getWidth() ; i++){
+			for(int j = 0; j<this.getHeight()-motif.getHeight() ; j++){
+				int ssd = this.SSD(i, j, motif);
+				if(min>ssd){
+					res[0]=i; res[1]=j;
+					min=ssd;
+				}
+			}
+		}
+		return res;
 	}
 }
