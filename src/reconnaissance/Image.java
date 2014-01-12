78afcd6cdf67d7da4b97b64bc08a44055dab5821
SiriusXIV <siriusxiv0@gmail.com>
2014-01-12 18:23:09 +0100

    Olivier

--- src/reconnaissance/Image.java
+++ src/reconnaissance/Image.java
@@ -11,9 +11,9 @@ import ij.gui.NewImage;
  *
  */
 public class Image {
-	
+
 	private ImagePlus image;
-	
+
 	/**
 	 * Création d'une image vierge à partir de ses dimensions
 	 * @param w largeur
@@ -45,7 +45,7 @@ public class Image {
 	private static int toGrey(int[] rgb){
 		return (int) (rgb[0]*0.299+rgb[1]*0.587+rgb[2]*0.114);
 	}
-	
+
 	/**
 	 * Accesseur
 	 * @return la largeur de l'image
@@ -60,7 +60,7 @@ public class Image {
 	public int getHeight() {
 		return image.getHeight();
 	}
-	
+
 	/**
 	 * "Allume" le pixel de coordonnees (x,y) avec la valeur value.
 	 * de points traces
@@ -80,7 +80,7 @@ public class Image {
 	public int getPixel(int x, int y){
 		return image.getPixel(x, y)[0];
 	}
-	
+
 	/**
 	 * Calcule la norme entre le motif et l'image au point x et y.
 	 * @param x
@@ -98,7 +98,8 @@ public class Image {
 		}
 		return res;
 	}
-	
+
+
 	/**
 	 * Renvoie la position sur l'image où le motif matche le plus.
 	 * @param motif
@@ -119,4 +120,47 @@ public class Image {
 		}
 		return res;
 	}
+
+	/**
+	 * Question 2.2
+	 * distX = norme de X, distP = norme de P; distPX = produit scalaire entre X et P
+	 * @param x
+	 * @param y
+	 * @param motif
+	 * @return
+	 */
+	public int SSD2(int x, int y, Motif motif){
+		int res = 0;int distX=0; int distP=0, distPX=0;
+		for(int i = 0; i<motif.getWidth() ; i++){
+			for(int j = 0; j<motif.getHeight() ; j++){
+				distX = distX+ (this.getPixel(i, j)*this.getPixel(x+i, y+j));
+				distP= distP+ motif.getPixel(i, j)*motif.getPixel(x+i, y+j);
+				distPX= distPX + motif.getPixel(i, j)*this.getPixel(x+i, y+j);		
+			}
+		}
+		res = distX*distX + distP*distP - 2*distPX;
+		return res;
+	}
+
+	public int[] minSSD2(Motif motif){
+		int[] res = new int[2];
+		res[0]=0; res[1]=0;
+		int min= this.SSD2(0, 0, motif);
+		int N = this.getHeight();
+		int M = this.getWidth();
+		int[][] Sn = new int[N][M];
+		for (int i=0; i<=N;i++){
+			for( int j=0; j<=M;j++){
+				for (int k=0; k<=motif.getWidth();k++) {Sn[i][j]+= motif.getPixel(i, j)*motif.getPixel(i, j+k);}
+			}
+		}
+		int[][] Smn = new int[N][M];
+		for (int i=0; i<=N;i++){
+			for( int j=0; j<=M;j++){
+				for (int k=0;k<=motif.getHeight();k++){Smn[i][j]+=Sn[i+k][j];}
+			}
+		}
+	}
+	//reste � trouver le minimum de Smn[i][j] et �a donnera la position de la forme qu'on cherche ??
+	//C'est �a ou pas ? Help !!!
 }
